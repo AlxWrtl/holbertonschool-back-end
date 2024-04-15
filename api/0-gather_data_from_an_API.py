6d7916b3f8096f9f8e9787851b982174acbe47da
AlxWrtl <indexes-benzine0p@icloud.com>
2024-04-15 15:14:47 +0200

    change the name in upercase

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,46 +10,46 @@ import requests
 import sys
 
 
-def fetch_employee_todo_progress(employee_id):
+def FETCH_EMPLOYEE_TODO_PROGRESS(EMPLOYEE_ID):
     """
     Fetches and prints the TODO list progress for a specific employee.
 
     Args:
-    employee_id (int): The ID of the employee to query.
+    EMPLOYEE_ID (int): The ID of the employee to query.
 
     Outputs:
     Prints the progress of employee's TODO tasks in the specified format.
     """
     # Endpoint URLs
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={
-        employee_id}"
+    USER_URL = f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}"
+    TODOS_URL = f"https://jsonplaceholder.typicode.com/todos?userId={
+        EMPLOYEE_ID}"
 
     # Fetch user data
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
+    USER_RESPONSE = requests.get(USER_URL)
+    USER_DATA = USER_RESPONSE.json()
+    EMPLOYEE_NAME = USER_DATA.get('name')
 
     # Fetch todos data
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
+    TODOS_RESPONSE = requests.get(TODOS_URL)
+    TODOS_DATA = TODOS_RESPONSE.json()
 
     # Calculate task completion statistics
-    total_tasks = len(todos_data)
-    completed_tasks = [todo for todo in todos_data if todo.get('completed')]
+    TOTAL_TASKS = len(TODOS_DATA)
+    COMPLETED_TASKS = [todo for todo in TODOS_DATA if todo.get('completed')]
 
     # Output the formatted task list
-    print(f"Employee {employee_name} is done with tasks({
-          len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks({
+          len(COMPLETED_TASKS)}/{TOTAL_TASKS}):")
+    for task in COMPLETED_TASKS:
         print(f"\t {task.get('title')}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         try:
-            employee_id = int(sys.argv[1])
-            fetch_employee_todo_progress(employee_id)
+            EMPLOYEE_ID = int(sys.argv[1])
+            FETCH_EMPLOYEE_TODO_PROGRESS(EMPLOYEE_ID)
         except ValueError:
             print("Please provide a valid integer for the employee ID.")
     else:
